9deac21293af97be72430d6c77703fc3e6520787
yungFundamental <jojo.barzilai@gmail.com>
2022-02-02 12:45:03 +0200

    Added collision function.

--- src/main/java/com/example/genetic_bird/Bird.java
+++ src/main/java/com/example/genetic_bird/Bird.java
@@ -7,13 +7,15 @@ import java.util.EventListener;
 public class Bird
 {
     private double y;           // current y
+    private final double x;     // x coordinate (doesnt change)
     private double velocity;    // current y axis velocity
     // the "hit box" of the bird is a rectangle:
-    private double height;
-    private double width;
+    private final double height;
+    private final double width;
 
-    public Bird(double _y, double _height, double _width)
+    public Bird(double _x, double _y, double _height, double _width)
     {
+        x = _x;
         y = _y;
         width = _width;
         height = _height;
@@ -54,6 +56,28 @@ public class Bird
     {
         // aka set velocity
         velocity = vel;
+    }
+
+
+    public boolean collisionCheck(PipePair p)
+    {
+        // X AXIS:
+        // compare left side of pipes and right side of bird
+        if (p.getX() > this.width + this.x)
+            return false;
+
+        // compare right side of pipes and left side of bird
+        if (p.getX() + p.getWidth() > this.x)
+            return false;
+
+        // Y AXIS:
+        // compare top of lower pipe and bottom of bird
+        if (p.getLowerPipe().getY() < this.y + this.height)
+            return true;
+
+        // compare bottom of upper pipe and top of bird
+        // (if  the following condition is met the objects collided, else they did not.)
+        return p.getUpperPipe().getY() + p.getUpperPipe().getHeight() > this.y;
     }
 
 }
